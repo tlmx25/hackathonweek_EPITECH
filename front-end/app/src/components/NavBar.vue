27063e75174455d9392597c93e1af73ef8ca9e66
justine <justine.loizel@gmail.com>
2024-01-16 13:35:10 +0100

    fix: navBar button changed to Espace Adminsitrateur

--- front-end/app/src/components/NavBar.vue
+++ front-end/app/src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-x5 bg-body-tertiary" sticky-top style="background-color: #19191b !important;">
     <div class="container-fluid" style="margin: 10px;">
       <img src="@/assets/6tm.svg" alt="logo">
-      <button type="button" class="btn btn-outline-light">Log in</button>
+      <button type="button" class="btn btn-outline-light">Espace administrateur</button>
     </div>
   </nav>
 </template>
