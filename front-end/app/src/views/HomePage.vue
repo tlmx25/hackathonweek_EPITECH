171f74b221768613044c94b9b6b9b6ebdf8562b2
justine <justine.loizel@gmail.com>
2024-01-16 12:34:58 +0100

    refacto: IntroText in separate component

--- front-end/app/src/views/HomePage.vue
+++ front-end/app/src/views/HomePage.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="container mt-5">
-        <div class="text-center">
-            <h2 class="mb-4" style="font-weight: bold;">Les <span style="color: #52c4a7;">belles personnes</span> qui composent 6TM</h2>
-            <h5>Découvrez notre équipe de plus de <span style="font-weight: bold;">100 experts</span> répartie entre Rennes et Angers.</h5>
-        </div>
+        <IntroText />
         <div class="row">
             <div class="col-md-3">
                 <SearchBar />
@@ -20,14 +17,16 @@
 
 import PeopleCards from '@/components/PeopleCards.vue'
 import FilterCards from '@/components/FilterCards.vue'
-import SearchBar from '../components/SearchBar.vue';
+import SearchBar from '@/components/SearchBar.vue';
+import IntroText from '@/components/IntroText.vue';
 
 export default {
     name: 'HomePage',
     components: {
         PeopleCards,
         FilterCards,
-        SearchBar
+        SearchBar,
+        IntroText
     }
 }
 
