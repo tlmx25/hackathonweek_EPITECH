f6a939898eee1922b6c37453750347ca750bffba
Quentin <quentin.bollore@epitech.eu>
2024-01-17 11:32:22 +0100

    feat: Pie Graph init

---
+++ front-end/app/src/components/admin/ThePieGraph.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="">
+        <div class="card p-3" style="width: 100%;">
+            <div id="chartdiv2"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import * as am5 from "@amcharts/amcharts5";
+import * as am5percent from "@amcharts/amcharts5/percent";
+
+export default {
+    data() {
+        return {
+            postes: [],
+            agences: [],
+            equipes: [],
+        }
+    },
+    mounted() {
+        let root = am5.Root.new("chartdiv2");
+
+// Set themes
+// https://www.amcharts.com/docs/v5/concepts/themes/
+root.setThemes([
+  am5themes_Animated.new(root)
+]);
+
+// Create chart
+// https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+let chart = root.container.children.push(
+  am5percent.PieChart.new(root, {
+    endAngle: 270
+  })
+);
+
+// Create series
+// https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+let series = chart.series.push(
+  am5percent.PieSeries.new(root, {
+    valueField: "value",
+    categoryField: "category",
+    endAngle: 270
+  })
+);
+
+series.states.create("hidden", {
+  endAngle: -90
+});
+
+// Set data
+// https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
+series.data.setAll([{
+  category: "Lithuania",
+  value: 501.9
+}, {
+  category: "Czechia",
+  value: 301.9
+}]);
+
+series.appear(1000, 100);
+    }
+}
+
+</script>
+
+<style scoped>
+#chartdiv2 {
+    width: 100%;
+    height: 250px;
+  }
+</style>
