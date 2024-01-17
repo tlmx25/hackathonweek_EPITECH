4561552cb627b34128f0c64ecb3aa66e4d871ee1
Quentin <quentin.bollore@epitech.eu>
2024-01-17 11:34:02 +0100

    feat: Admin home page with graph

---
+++ front-end/app/src/components/admin/TheAdminHome.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="chart">
+        <div class="" style="flex: 1;">
+            <PieGraph />
+        </div>
+        <div class="" style="flex: 2;">
+            <CurvedGraph />
+        </div>
+    </div>
+    <div class="mt-3">
+        <AllGraph />
+    </div>
+</template>
+
+<script>
+import AllGraph from "./TheAllGraph.vue"
+import PieGraph from "./ThePieGraph.vue"
+import CurvedGraph from "./TheCurvedGraph.vue"
+
+export default {
+    components: {
+        AllGraph,
+        PieGraph,
+        CurvedGraph,
+    },
+    data() {
+        return {
+            postes: [],
+            agences: [],
+            equipes: [],
+        }
+    },
+    methods: {
+    },
+}
+</script>
+
+<style scoped>
+.chart {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+}
+.mt-5 {
+    width: 100%;
+}
+</style>
