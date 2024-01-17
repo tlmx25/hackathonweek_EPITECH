aee1b30d637d194320c2187014099542951cb3d2
Quentin <quentin.bollore@epitech.eu>
2024-01-17 11:31:22 +0100

    feat: Admin view for back office

--- front-end/app/src/views/AdminView.vue
+++ front-end/app/src/views/AdminView.vue
@@ -2,15 +2,20 @@
   <template v-if="selectedElement == 'Trombi'">
     <TheCard />
   </template>
+  <template v-if="selectedElement == 'Home'">
+    <AdminHome />
+  </template>
 </template>
 
 <script>
-import TheCard from "@/components/TheCard.vue";
+import TheCard from "@/components/admin/TheCard.vue";
+import AdminHome from "@/components/admin/TheAdminHome.vue"
 import { mapState } from "vuex";
 
 export default {
   components: {
     TheCard,
+    AdminHome
   },
   computed: {
     ...mapState(["selectedElement"]),
