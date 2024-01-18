9580875a4e2d6d09f2ccb2579ab50b5c7fa70f74
Tom <tom.lefoix@epitech.eu>
2024-01-18 18:41:08 +0100

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-docker compose up --build -d
 
 if [[ "$1" == "--init" ]]; then
-    docker exec -it backend_web bash -c "yes | php bin/console doctrine:database:create || yes | php bin/console make:migration || yes | php bin/console doctrine:migrations:migrate"
+    docker exec -it backend_web bash -c "yes | php bin/console doctrine:database:create ; yes | php bin/console make:migration ; yes | php bin/console doctrine:migrations:migrate"
     curl http://localhost:8000/init
 else
     echo "You can init the database using the --init."
